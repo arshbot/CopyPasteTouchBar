12ca0adb5fa59b5c42b1afff1056dbf750d4d71e
Harsha Goli <harshagoli@gmail.com>
2017-09-03 21:54:32 -0400

    Last commit

---
+++ TouchBar/TouchBarIdentifiers.swift
@@ -0,0 +1,9 @@
+//
+//  TouchBarIdentifiers.swift
+//  TouchBar
+//
+//  Created by Andy Pereira on 10/31/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
---
+++ TouchBar/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  TouchBar
+//
+//  Created by Chris Ricker on 10/30/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
